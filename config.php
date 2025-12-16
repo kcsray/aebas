5abf601f2012991b4e2a6a2f438d8f8478ab2953
Kiranchand Samantaray <kcsray@yahoo.com>
2025-12-16 07:30:15 +0000

    Update file config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ define('DB_USERNAME', 'if0_40578902');
 define('DB_PASSWORD', 'Github123AXN');
 define('DB_NAME', 'if0_40578902_nicaebas');
 
-$host = "sql100.infinityfree.com";
+$host = 'sql100.infinityfree.com';
 $dbname = 'if0_40578902_nicaebas';
 $username = 'if0_40578902'; // Replace with your database username
 $password = 'Github123AXN'; // Replace with your database password
