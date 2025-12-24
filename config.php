45de724bc346adc810133b269e57da6f83726e86
kcsray <kcsray@yahoo.com>
2025-12-24 14:22:13 +0530

    Modify config file

--- config.php
+++ config.php
@@ -13,12 +13,12 @@ $username = 'if0_40578902'; // Replace with your database username
 $password = 'Github123AXN'; // Replace with your database password
 $port = '3306'; // Separate port specification
 
- 
 /* Attempt to connect to MySQL database */
 try{
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e){
     die("ERROR: Could not connect. " . $e->getMessage());
 }
