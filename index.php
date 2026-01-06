d5725fe522a34601e455ae3d2143b9761154bb16
kcsray <kcsray@yaho.com>
2026-01-06 18:02:53 +0530

    Demo to Jyoti P Behera

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AEBAS | Device Management System</title>
+    <title>AEBAS | Device Management System -- By Kiranchand </title>
     
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
